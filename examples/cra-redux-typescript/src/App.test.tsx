eb7071a92af16fc39194d8c2ee492be36b17e709
Lennard Scheibel <contact@lennardscheibel.de>
2022-03-08 20:15:54 +0100

    Add test back to cra-redux-typescript example

--- examples/cra-redux-typescript/src/App.test.tsx
+++ examples/cra-redux-typescript/src/App.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
 
-test('renders learn react link', () => {
-  // Todo: Setup jest properly
-  // const { getByText } = render(
-  //   <Provider store={store}>
-  //     <App />
-  //   </Provider>
-  // );
-  //
-  // expect(getByText(/learn/i)).toBeInTheDocument();
+test('renders without errors', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(getByText(/learn/i)).toBeInTheDocument();
 });
